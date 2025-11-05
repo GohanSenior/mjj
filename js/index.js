14e5bd724e725ea5cb5cb0c047c21362eba7e18d
Jason <Narberal766@gmail.com>
2025-11-05 10:47:45 +0100

    Ajout des poster via JavaScript qui va chercher dans le JSON

--- js/index.js
+++ js/index.js
@@ -2,12 +2,24 @@
 
 const URLDATA = "./data/data.json";
 
-
+let carouDiv = document.getElementById("createcarou");
 
 fetch(URLDATA)
-    .then((response) => response.json())
-    .then((data) => {
-        // console.log(data.vote_average);
-        console.log(data);
+    .then(response => response.json())  // Récupérer les données JSON
+    .then(data => {
+
+        // Limiter à 3 films
+        const films = data.results.slice(0, 3);  
+
+        // Créer les items du carrousel et les indicateurs dynamiquement
+            
+            films.forEach((film, index) => {
+                carouDiv.innerHTML += `<div class="carousel-item ${index === 0 ? 'active' : ''}">
+                    <img src="https://image.tmdb.org/t/p/w500${film.poster_path}" class="d-block m-auto" alt="...">
+                </div>`;
+            });
+        
     })
-    .catch((error) => console.error("Erreur:", error));
+    .catch(error => {
+        console.error("Erreur:", error);
+    });
